8eb4a5803d953a9cd9d0e5ed9363201a638084d0
WINEZERO <kissloo@foxmail.com>
2024-06-11 01:43:54 +0800

    添加http请求代码，暂未使用

---
+++ src/main/java/utils/BurpHttpUtils.java
@@ -0,0 +1,71 @@
+package utils;
+
+import burp.BurpExtender;
+import burp.IHttpRequestResponse;
+import burp.IHttpService;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
+
+public class BurpHttpUtils {
+    public static int MaxResponseContentLength = 500000;
+
+    public static Map<String, Object> makeGetRequest(Map<String, Object> pathDataModel) {
+        Map<String, Object> onePathData = (Map<String, Object>) pathDataModel.get("path_data");
+        onePathData.put("path", pathDataModel.get("path"));
+        onePathData.put("url", pathDataModel.get("url"));
+        // 解析URL
+        String host = (String) onePathData.get("host");
+        // 使用Number作为中间类型，以应对可能不同的数字类型
+        Number portNumber = (Number) onePathData.get("port");
+        int port = portNumber.intValue();
+        String protocol = (String) onePathData.get("protocol");
+        String path = (String) onePathData.get("path");
+        // 创建IHttpService对象
+        IHttpService httpService = BurpExtender.getHelpers().buildHttpService(host, port, protocol);
+
+        // 构造GET请求的字节数组
+        String request = "GET " + path + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36" + "\r\n" +
+                "\r\n";
+        byte[] requestBytes = request.getBytes();
+
+        // 初始化返回数据结构
+        onePathData.put("method", "GET");
+        onePathData.put("time", new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+
+        IHttpRequestResponse requestResponse = null;
+
+        try {
+            // 发起请求
+            requestResponse = BurpExtender.getCallbacks().makeHttpRequest(httpService, requestBytes);
+            // 空检查
+            if (requestResponse == null || requestResponse.getResponse() == null) {
+                throw new IllegalStateException("Request failed, no response received.");
+            }
+
+            // 获取响应字节
+            byte[] responseBytes = requestResponse.getResponse();
+            responseBytes = responseBytes.length  > MaxResponseContentLength ? Arrays.copyOf(responseBytes, MaxResponseContentLength) : responseBytes;
+            String statusCode = String.valueOf(BurpExtender.getCallbacks().getHelpers().analyzeResponse(responseBytes).getStatusCode());
+
+            // 添加请求和响应数据到返回数据结构
+            onePathData.put("requests", Base64.getEncoder().encodeToString(requestBytes));
+            onePathData.put("response", Base64.getEncoder().encodeToString(responseBytes));
+            onePathData.put("status", statusCode);
+        } catch (Exception e) {
+            // 异常处理，记录错误信息
+            onePathData.put("status", "请求报错");
+            onePathData.put("requests", Base64.getEncoder().encodeToString(requestBytes));
+            onePathData.put("response", Base64.getEncoder().encodeToString(e.getMessage().getBytes()));
+        }
+
+        return onePathData;
+
+    }
+}
