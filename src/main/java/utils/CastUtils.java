37130ddce744e4851b773fa4eb360cab5253a1c3
WINEZERO <kissloo@foxmail.com>
2024-07-15 14:34:19 +0800

    实现响应提取函数

--- src/main/java/utils/CastUtils.java
+++ src/main/java/utils/CastUtils.java
@@ -3,9 +3,12 @@ package utils;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import model.HttpUrlInfo;
+import utilbox.HelperPlus;
 
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.CRC32;
 
 public class CastUtils {
@@ -203,6 +206,13 @@ public class CastUtils {
     public static String calcCRC32(String string) {
         // 使用 UTF-8 编码将字符串转换为字节数组
         byte[] inputBytes = string.getBytes(StandardCharsets.UTF_8);
+        return calcCRC32(inputBytes);
+    }
+
+    /**
+     * 字符Byte[]转 CRC32
+     */
+    public static String calcCRC32(byte[] inputBytes) {
         // 初始化CRC32对象
         CRC32 crc32 = new CRC32();
         // 更新CRC值
@@ -259,5 +269,45 @@ public class CastUtils {
         return new ArrayList<>(rootUrlSet);
     }
 
+
+    /**
+     * 解析URL获取 302 URL
+     * @param headerBytes
+     * @return
+     */
+    public static String parseRespRedirectUrl(byte[] headerBytes) {
+        String redirectUrl = null;
+        if (headerBytes.length>0) {
+            HelperPlus helperPlus = new HelperPlus();
+            redirectUrl = helperPlus.getHeaderValueOf(true, headerBytes, "Location");
+        }
+        return redirectUrl;
+    }
+
+    /**
+     * 从HTML文档中提取<title>标签的内容。
+     */
+    public static String parseTextTitle(byte[] bodyBytes) {
+        String title = null;
+        if (bodyBytes.length>0){
+            byte[] bytesToParse = bodyBytes;
+            // 如果bodyBytes长度大于10000，仅取前10000字节
+            if (bodyBytes.length > 10000) {
+                bytesToParse = Arrays.copyOfRange(bodyBytes, 0, 10000);
+            }
+            // 将字节数组转换为字符串
+            String htmlContent = new String(bytesToParse, StandardCharsets.UTF_8);
+            // 定义一个正则表达式来匹配<title>标签内的内容
+            Pattern pattern = Pattern.compile("<title>(.*?)</title>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+            // 创建一个Matcher对象
+            Matcher matcher = pattern.matcher(htmlContent);
+            // 检查是否找到了匹配项
+            if (matcher.find()) {
+                title = matcher.group(1).trim();
+            }
+        }
+
+        return title;
+    }
 
 }
