1b4be356a58e5ca3453e322313d9f7347bc00526
WINEZERO <kissloo@foxmail.com>
2024-07-19 17:27:51 +0800

    删除不需要的变量

--- src/main/java/database/Constants.java
+++ src/main/java/database/Constants.java
@@ -1,22 +1,11 @@
 package database;
 
 public class Constants {
-    public static final String ANALYSE_WAIT = "等待解析";
-    public static final String ANALYSE_ING = "解析中";
-    public static final String ANALYSE_END = "解析完成";
-    public static final String ANALYSE_SKIP = "无需解析";
+    public static final String ANALYSE_WAIT = "Waiting";  //等待处理
+    public static final String ANALYSE_ING = "Running";  //处理中
+    public static final String ANALYSE_END = "Finished"; //处理完毕
+    public static final String ANALYSE_SKIP = "Skipped";  //跳过处理
 
-    public static final String MSG_HASH = "MSG_HASH";
     public static final String SPLIT_SYMBOL = "<->";
-    public static final String REQ_HOST_PORT = "REQ_HOST_PORT";
-    public static final String REQ_PATH_DIRS = "REQ_PATH_DIRS";
-    public static final String PATH_TREE = "PATH_TREE";
-    public static final String BASIC_PATH_NUM = "BASIC_PATH_NUM";
-
-    public static final String RESP_STATUS_CODE = "RESP_STATUS_CODE";
-    public static final String REQ_BASE_URL = "REQ_BASE_URL";
-    public static final String REQ_PATH_EXT = "REQ_PATH_EXT";
-    public static final String REQ_PROTO = "REQ_PROTO";
-
     public static final String RULE_CONF_PREFIX = "CONF_"; //配置文件中 配置规则的开头
 }
