e465fa85b01061b1aaf915da2d8fa8483711d126
WINEZERO <kissloo@foxmail.com>
2024-08-08 00:50:41 +0800

    rename

--- src/main/java/burp/IProxyScanner.java
+++ src/main/java/burp/IProxyScanner.java
@@ -32,9 +32,9 @@ public class IProxyScanner implements IProxyListener {
     public static Map<String, Map<String,Object>> urlCompareMap = new HashMap<>(); //存储每个域名的对比关系,后续可以考虑写入到数据库
     private static ConcurrentHashMap<String, Map<String,Object>> notCompareMap = new ConcurrentHashMap<>();  //在域名对比关系生成前,需要把响应信息先存起来,等后续再进行处理
 
-    public static boolean dynamicPathFilterIsOpen = true;    //是否启用增强的path过滤模式
+    public static boolean dynamicPathFilterIsOpen = true;    //是否启用增强的path过滤模式 //需要设置默认关闭,暂时功能没有完善、对于URL无法访问的情况没有正常处理、导致卡顿
 
-    public static boolean autoRecordPathIsOpen  = true;     //是否启用自动记录每个录得PATH
+    public static boolean autoRecordPathIsOpen  = true;     //是否启用自动记录每个录得PATH //自动记录功能应该开启,不然没有pathTree生成
     public static boolean autoPathsToUrlsIsOpen = false;    //是否进行自动PathTree生成URL
     public static boolean autoRecursiveIsOpen = false;      //是否进行递归URL扫描
 
@@ -263,8 +263,8 @@ public class IProxyScanner implements IProxyListener {
     /**
      * 增强的有效路径判断过滤函数
      */
-    public static void enhanceRecordPathFilter(HttpMsgInfo msgInfo, boolean dynamicPathFilterIsOpen) {
-        if (!dynamicPathFilterIsOpen){
+    public static void enhanceRecordPathFilter(HttpMsgInfo msgInfo, boolean openDynamicFilter) {
+        if (!openDynamicFilter){
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
