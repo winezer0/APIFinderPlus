ec685ad0f886cea00d54ef8f1f8713c666cc5677
WINEZERO <kissloo@foxmail.com>
2024-08-23 20:01:59 +0800

    添加一列 运行状态 备选

--- src/main/java/database/AnalyseUrlResultTable.java
+++ src/main/java/database/AnalyseUrlResultTable.java
@@ -21,31 +21,31 @@ public class AnalyseUrlResultTable {
 
     //创建用于存储 需要处理的URL的原始请求响应
     static String creatTableSQL  = "CREATE TABLE IF NOT EXISTS "+ tableName +" (\n"
-            + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-            + " msg_hash TEXT UNIQUE,\n"  //请求Hash信息
-            + " req_url TEXT NOT NULL,\n"  //请求URL
-            + " req_host_port TEXT NOT NULL,\n"  //请求HOST PORT
+            + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+            + "msg_hash TEXT UNIQUE,\n"  //请求Hash信息
+            + "req_url TEXT NOT NULL,\n"  //请求URL
+            + "req_host_port TEXT NOT NULL,\n"  //请求HOST PORT
 
-            + " find_url TEXT DEFAULT '',\n"    //分析出来的URL信息 (Json格式)
-            + " find_url_num INTEGER DEFAULT -1,\n"     //发现URL的数量
+            + "find_url TEXT DEFAULT '',\n"    //分析出来的URL信息 (Json格式)
+            + "find_url_num INTEGER DEFAULT -1,\n"     //发现URL的数量
 
-            + " find_path TEXT DEFAULT '',\n"   //分析出来的URI信息 还需要补充路径 (Json格式)
-            + " find_path_num INTEGER DEFAULT -1,\n"    //发现PATH的数量
+            + "find_path TEXT DEFAULT '',\n"   //分析出来的URI信息 还需要补充路径 (Json格式)
+            + "find_path_num INTEGER DEFAULT -1,\n"    //发现PATH的数量
 
-            + " find_info TEXT DEFAULT '',\n"   //分析出来的敏感信息(Json格式)
-            + " find_info_num INTEGER DEFAULT -1,\n"    //发现INFO的数量
-            + " has_important INTEGER DEFAULT 0,\n"    //是否存在重要信息
+            + "find_info TEXT DEFAULT '',\n"   //分析出来的敏感信息(Json格式)
+            + "find_info_num INTEGER DEFAULT -1,\n"    //发现INFO的数量
+            + "has_important INTEGER DEFAULT 0,\n"    //是否存在重要信息
 
-            + " find_api TEXT DEFAULT '',\n"        //基于分析的不完整URI信息 直接拼接 出来的URL (Json格式)
-            + " find_api_num INTEGER DEFAULT -1,\n"     //发现API的数量
+            + "find_api TEXT DEFAULT '',\n"        //基于分析的不完整URI信息 直接拼接 出来的URL (Json格式)
+            + "find_api_num INTEGER DEFAULT -1,\n"     //发现API的数量
 
-            + " path_to_url TEXT DEFAULT '',\n"      //基于分析的不完整URI信息 智能计算 出来的URL (Json格式)
-            + " path_to_url_num INTEGER DEFAULT -1,\n"     //发现API的数量
+            + "path_to_url TEXT DEFAULT '',\n"      //基于分析的不完整URI信息 智能计算 出来的URL (Json格式)
+            + "path_to_url_num INTEGER DEFAULT -1,\n"     //发现API的数量
 
-            + " unvisited_url TEXT DEFAULT '',\n"      //合并所有URL 并去除已经访问过的URL (Json格式)
-            + " unvisited_url_num INTEGER DEFAULT -1,\n"   //合并所有URL 并去除已经访问过的URL的数量
+            + "unvisited_url TEXT DEFAULT '',\n"      //合并所有URL 并去除已经访问过的URL (Json格式)
+            + "unvisited_url_num INTEGER DEFAULT -1,\n"   //合并所有URL 并去除已经访问过的URL的数量
 
-            + " basic_path_num INTEGER DEFAULT -1,\n"     //是基于多少个路径算出来的结果?
+            + "basic_path_num INTEGER DEFAULT -1,\n"     //是基于多少个路径算出来的结果?
 
             + "run_status TEXT NOT NULL DEFAULT 'ANALYSE_WAIT'"
             .replace("ANALYSE_WAIT", Constants.ANALYSE_WAIT)
