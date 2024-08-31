071668ad896870bac6db077cfa54aea098643a31
WINEZERO <kissloo@foxmail.com>
2024-08-31 10:54:32 +0800

    rename

--- src/main/java/database/Constants.java
+++ src/main/java/database/Constants.java
@@ -1,11 +1,11 @@
 package database;
 
 public class Constants {
-    public static final String ANALYSE_WAIT = "Waiting";     //等待处理
-    public static final String ANALYSE_ING = "Analysing";    //处理中
-    public static final String ANALYSE_END = "Analysed";    //处理完毕
+    public static final String ANALYSE_WAIT = "Waiting";     //等待自动处理
+    public static final String ANALYSE_ING = "Analysing";    //自动处理中
+    public static final String ANALYSE_END = "Analysed";    //自动处理完毕
 
-    public static final String HANDLE_WAIT = "Waiting";  //手动处理中
+    public static final String HANDLE_WAIT = "Pending";  //等待手动处理
     public static final String HANDLE_ING = "Handling";  //手动处理中
     public static final String HANDLE_END = "Handled";   //手动处理完毕
 
